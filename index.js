2b7862c471698dd91e5c7419eb5c98e215f4dbc2
demoaccount-rishav <150954464+demoaccount-rishav@users.noreply.github.com>
2024-05-29 10:56:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello mckv')
+  res.send('!! Welcome To SDIO Lab Exam 2024 !!')
 })
 
 app.listen(4000, ()=>{
